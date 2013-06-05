7675b40571038b3009e0a81889f4fee1f4109707
Kyle Brougham <brougham.kyle@gmail.com>
2013-06-05 10:39:22 -0400

    Get/sets on door class

--- SunspaceDealerDesktop/Door.cs
+++ SunspaceDealerDesktop/Door.cs
@@ -8,14 +8,102 @@ namespace SunspaceDealerDesktop
     public class Door
     {
         private String typeOfDoor; //Type of Door String
-        private bool isPatioDoor; //True if Patio Door ?????????????
+        //private bool isPatioDoor; //True if Patio Door ?????????????
         private int itemIndex; //LinearItems Array Index
         private int transomStyle; //Transom Style: 0-Solid, 1-Glass, 2-Vinyl , glass rect, vinyl rect
         private int transomTint;
-        private int underHeaderStyle; //Transom Style: 0-Solid, 1-Glass, 2-Vinyl , glass rect, vinyl rect
-        private int underHeaderTint; //?????????????????????????????????????????????????????????????????
+        //private int underHeaderStyle; //Transom Style: 0-Solid, 1-Glass, 2-Vinyl , glass rect, vinyl rect
+        //private int underHeaderTint; //?????????????????????????????????????????????????????????????????
         private float height; //Punch Height of Door
         private float width; //Door Width (Not including Frame)
+
+        public Door()
+        {
+            TypeOfDoor = "Entry"; 
+            ItemIndex = -1;
+            TransomStyle = 1; 
+            TransomTint = 1;
+            Height = 80.5f; 
+            Width = 60.6f;
+        }
+
+        public String TypeOfDoor
+        {
+            get
+            {
+                return typeOfDoor;
+            }
+
+            set
+            {
+                typeOfDoor = value;
+            }
+        }
+       
+        public int ItemIndex
+        {
+            get
+            {
+                return itemIndex;
+            }
+
+            set
+            {
+                itemIndex = value;
+            }
+        }
+
+        public int TransomStyle
+        {
+            get
+            {
+                return transomStyle;
+            }
+
+            set
+            {
+                transomStyle = value;
+            }
+        }
+
+        public int TransomTint
+        {
+            get
+            {
+                return transomTint;
+            }
+
+            set
+            {
+                transomTint = value;
+            }
+        }
+
+        public float Height
+        {
+            get
+            {
+                return height;
+            }
+
+            set
+            {
+                height = value;
+            }
+        }
+
+        public float Width
+        {
+            get
+            {
+                return width;
+            }
+
+            set
+            {
+                width = value;
+            }
+        }
 
         /*
         bool patioXO; //??????????????
