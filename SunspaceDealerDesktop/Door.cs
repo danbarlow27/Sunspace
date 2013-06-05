37e34f26c0920fa64c37554e78983dcb02ac6e40
Kyle Brougham <brougham.kyle@gmail.com>
2013-06-05 10:29:31 -0400

    Door created without get/set

---
+++ SunspaceDealerDesktop/Door.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SunspaceDealerDesktop
+{
+    public class Door
+    {
+        private String typeOfDoor; //Type of Door String
+        private bool isPatioDoor; //True if Patio Door ?????????????
+        private int itemIndex; //LinearItems Array Index
+        private int transomStyle; //Transom Style: 0-Solid, 1-Glass, 2-Vinyl , glass rect, vinyl rect
+        private int transomTint;
+        private int underHeaderStyle; //Transom Style: 0-Solid, 1-Glass, 2-Vinyl , glass rect, vinyl rect
+        private int underHeaderTint; //?????????????????????????????????????????????????????????????????
+        private float height; //Punch Height of Door
+        private float width; //Door Width (Not including Frame)
+
+        /*
+        bool patioXO; //??????????????
+        int glassTint; 
+        bool cabanaSwingOut;
+        bool cabanaLHH;
+        bool boxHeader;
+        bool boxHeader2;
+        float kneewallHeight;
+        int hardware;
+        int ventCount;
+        bool customWidth;
+        bool customHeight;
+        bool doubleTransom;
+        bool temperedTransom;
+        String mixedTint;
+        bool isFrench;
+        bool priLeft;
+        bool hasGrills;
+        int sunShade; //1 = control Left, 2 = control right
+        */
+    }
+}
