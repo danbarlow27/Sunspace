b9978f2f3d7be2400923448353d5f76efb72c964
Kyle Brougham <brougham.kyle@gmail.com>
2013-06-05 10:54:08 -0400

    Receiver accessors added

--- SunspaceDealerDesktop/Receiver.cs
+++ SunspaceDealerDesktop/Receiver.cs
@@ -8,8 +8,40 @@ namespace SunspaceDealerDesktop
     public class Receiver
     {
         int itemIndex; //LinearItems Array Index
-        float cutLength; //Length to Cut Starter At ?????????????????
-        String Colour; //Colour of the receiver
-        bool isTwoPiece; //???????????
+        //float cutLength; //Length to Cut Starter At ?????????????????
+        String colour; //Colour of the receiver
+        //bool isTwoPiece; //???????????
+
+        public Receiver()
+        {
+            ItemIndex = -1;
+            Colour = "red";
+        }
+
+        public int ItemIndex
+        {
+            get
+            {
+                return itemIndex;
+            }
+
+            set
+            {
+                itemIndex = value;
+            }
+        }
+
+        public String Colour
+        {
+            get
+            {
+                return colour;
+            }
+
+            set
+            {
+                colour = value.ToLower();
+            }
+        }
     }
 }
