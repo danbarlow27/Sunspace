f00f0acff5a313c14a83c1347a5610cf0d3a5ac8
Kyle Brougham <brougham.kyle@gmail.com>
2013-07-09 14:00:33 -0400

    Add user successful

--- SunspaceDealerDesktop/AddUsers.aspx.cs
+++ SunspaceDealerDesktop/AddUsers.aspx.cs
@@ -29,34 +29,67 @@ namespace SunspaceDealerDesktop
                 Response.Redirect("Home.aspx");
             }
 
-            //if >-1 it cannot be a sunspace user, so we hide controls accordingly
-            if (Convert.ToInt32(Session["dealer_id"].ToString()) > -1)
+            if (!IsPostBack)
             {
-                UserTypeDiv.Visible = false;
-                UserGroupDiv.Visible = false;
-                userType = "D";
-            }
-            //sunspace CSR
-            else if (Convert.ToInt32(Session["dealer_id"].ToString()) == -1 && Session["user_group"].ToString() == "C")
-            {
-                UserTypeDiv.Visible = false;
-                ddlUserGroup.Items.Add("Admin");
-                ddlUserGroup.Items.Add("Sales Rep");
-                userType = "S";
-            }
-            //sunspace admin
-            else
-            {
-                ddlUserType.Items.Add("Sunspace");
-                ddlUserType.Items.Add("Dealer");
+                //if >-1 it cannot be a sunspace user, so we hide controls accordingly
+                if (Convert.ToInt32(Session["dealer_id"].ToString()) > -1)
+                {
+                    UserTypeDiv.Visible = false;
+                    UserGroupDiv.Visible = false;
+                    DealerListDiv.Visible = false;
+                    userType = "D";
+                }
+                else
+                {
+                    //sunspace CSR
+                    if (Convert.ToInt32(Session["dealer_id"].ToString()) == -1 && Session["user_group"].ToString() == "C")
+                    {
+                        UserTypeDiv.Visible = false;
+                        ddlUserGroup.Items.Add("Admin");
+                        ddlUserGroup.Items.Add("Sales Rep");
+                        userType = "S";
+                    }
+                    //sunspace admin
+                    else
+                    {
+                        DealerListDiv.Attributes.Add("style", "display: none;");
+                        ddlUserType.Items.Add("Sunspace");
+                        ddlUserType.Items.Add("Dealer");
 
-                //will alows land on sunspace by default, so we load with admin/csr on ddlusergroup
-                ddlUserGroup.Items.Add("Admin");
-                ddlUserGroup.Items.Add("Customer Service Rep");
-                userType = "S";
+                        //will alows land on sunspace by default, so we load with admin/csr on ddlusergroup
+                        ddlUserGroup.Items.Add("Admin");
+                        ddlUserGroup.Items.Add("Customer Service Rep");
+                        userType = "S";
+                    }
+                    //sunspace user, so populate dealer list
+
+                    //check to see if the list has already been retrieved
+                    if (Session["ddlDealers"] == null)
+                    {
+                        //Get the customers assosciated with this dealer
+                        sdsUsers.SelectCommand = "SELECT dealer_name FROM dealers ORDER BY dealer_name";
+
+                        //assign the table names to the dataview object
+                        DataView dvDealers = (DataView)sdsUsers.Select(System.Web.UI.DataSourceSelectArguments.Empty);
+
+                        ddlDealers.Items.Clear();
+
+                        for (int i = 0; i < dvDealers.Count; i++)
+                        {
+                            ddlDealers.Items.Add(dvDealers[i][0].ToString());
+                        }
+
+                        Session.Add("ddlDealers", ddlDealers);
+                    }
+                    //if it exists, just populate from session
+                    else
+                    {
+                        ddlDealers = (DropDownList)Session["ddlDealers"];
+                    }
+                }
+                // set usergroup for jscript access
+                userGroup = Session["user_group"].ToString();
             }
-            // set usergroup for jscript access
-            userGroup = Session["user_group"].ToString();
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)
@@ -89,24 +122,24 @@ namespace SunspaceDealerDesktop
                                             + txtLastName.Text + "', "
                                             + 1 + ")";
                     sdsUsers.Insert();
+                    lblError.Text = "Successfully Added";
                 }
+                //sunspace csr
+                else if (Convert.ToInt32(Session["dealer_id"].ToString()) == -1 && userGroup == "C")
+                {
+                    if (ddlUserGroup.SelectedValue == "Admin")
+                    {
+                        //create new dealer in dealer table
+                        //select new dealer
+                        //add user with reference to dealer table
+                    }
+                    //sales rep
+
+                }
+                //sunspace admin
                 else
                 {
-                    DateTime aDate = DateTime.Now;
-                    sdsUsers.InsertCommand = "INSERT INTO users (login, password, email_address, enrol_date, last_access, user_type, user_group, reference_id, first_name, last_name, status)"
-                                            + "VALUES('"
-                                            + txtLogin.Text + "', '"
-                                            + GlobalFunctions.CalculateMD5Hash(txtPassword.Text) + "', '"
-                                            + txtEmail.Text + "', '"
-                                            + aDate.ToString("yyyy/MM/dd") + "', '"
-                                            + aDate.ToString("yyyy/MM/dd") + "', '" //default to same-day
-                                            + ddlUserType.SelectedValue + "', '" //Must be D-S because a dealer can only add users of his dealership
-                                            + ddlUserGroup.SelectedValue + "', "
-                                            + Convert.ToInt32(Session["dealer_id"].ToString()) + ", '" //reference ID is the dealer id in the dealer table they belong to
-                                            + txtFirstName.Text + "', '"
-                                            + txtLastName.Text + "', "
-                                            + 1 + ")";
-                    sdsUsers.Insert();
+
                 }
             }
     //        div id="UserTypeDiv" runat="server">
