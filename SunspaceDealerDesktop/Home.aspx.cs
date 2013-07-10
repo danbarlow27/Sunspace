3b38a76f78563a5b8a7f2c9ae369f143fa5cddcc
Kyle Brougham <brougham.kyle@gmail.com>
2013-07-10 10:59:00 -0400

    Spoof page, edited addusers for usertype, home now preferences, added home page

--- SunspaceDealerDesktop/Home.aspx.cs
+++ SunspaceDealerDesktop/Home.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace SunspaceDealerDesktop
 {
-    public partial class Home : System.Web.UI.Page
+    public partial class Home1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -15,36 +15,13 @@ namespace SunspaceDealerDesktop
             {
                 //uncomment me when login functionality is working
                 Response.Redirect("Login.aspx");
-                //Session.Add("loggedIn", "userA");
+                //Session.Add("loggedIn", "1");
             }
-
-            #region Dropdown population
-
-            ListItem lst0 = new ListItem("---", "", true);
-            ListItem lst18 = new ListItem("1/8", ".125");
-            ListItem lst14 = new ListItem("1/4", ".25");
-            ListItem lst38 = new ListItem("3/8", ".375");
-            ListItem lst12 = new ListItem("1/2", ".5");
-            ListItem lst58 = new ListItem("5/8", ".625");
-            ListItem lst34 = new ListItem("3/4", ".75");
-            ListItem lst78 = new ListItem("7/8", ".875");
-
-            ddl100DefaultFiller.Items.Add(lst0);
-            ddl100DefaultFiller.Items.Add(lst18);
-            ddl100DefaultFiller.Items.Add(lst14);
-            ddl100DefaultFiller.Items.Add(lst38);
-            ddl100DefaultFiller.Items.Add(lst12);
-            ddl100DefaultFiller.Items.Add(lst58);
-            ddl100DefaultFiller.Items.Add(lst34);
-            ddl100DefaultFiller.Items.Add(lst78);
-
-            #endregion
         }
 
-        protected void btnUpdate_Click(object sender, EventArgs e)
+        protected void btnPreferences_Click(object sender, EventArgs e)
         {
-            //save all preferences
-            //additional comment here
+            Response.Redirect("Preferences.aspx");
         }
 
         protected void btnAddUsers_Click(object sender, EventArgs e)
