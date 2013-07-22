e1fdf8bf345cd7b9ce504b8f7a9c5ae26d46932c
unknown <joycelchan1@gmail.com>
2013-07-22 19:56:05 -0400

    updated error message label

--- SunspaceDealerDesktop/WizardWallsAndMods.aspx.designer.cs
+++ SunspaceDealerDesktop/WizardWallsAndMods.aspx.designer.cs
@@ -40,15 +40,6 @@ namespace SunspaceDealerDesktop {
         protected global::System.Web.UI.WebControls.Table tblProposedWalls;
         
         /// <summary>
-        /// Label1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-        
-        /// <summary>
         /// btnQuestion1 control.
         /// </summary>
         /// <remarks>
@@ -362,6 +353,15 @@ namespace SunspaceDealerDesktop {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblQuestion4PagerAnswer;
+        
+        /// <summary>
+        /// lblErrorMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblErrorMessage;
         
         /// <summary>
         /// hiddenFieldsDiv control.
