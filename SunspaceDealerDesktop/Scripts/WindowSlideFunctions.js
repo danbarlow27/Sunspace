730f72f8b79053dc3afe64488adb5f5c35d152ba
tamjd1 <taha.amjad@uoit.net>
2013-08-13 14:58:28 -0400

    window functions moved to WindowSlideFunctions.js

---
+++ SunspaceDealerDesktop/Scripts/WindowSlideFunctions.js
@@ -0,0 +1,134 @@
+﻿/************************************************************************************************************************************/
+/****************                 new function to be written                *******************************/
+/************************************************************************************************************************************/
+
+
+//do windows have to be precise to eighth of an inch also
+
+function fillWindowsMods() {
+
+    var availableSpaces = new Array();
+    var freeSpaceCounter = 0;
+
+    for (var i = 0; i < walls.length; i++) { //for each wall in the list of wall objects
+        if (coordList[i][4] === "P") { //if it is a proposed wall
+            if (walls[i].doors.length > 0) { //if there is at least 1 door in the wall
+                for (var j = 0; j < walls[i].doors.length; j++) {
+                    var freeSpace;
+                    if (walls[i].doors[j].position > 0) {
+                        freeSpace = {
+                            "wall": i,
+                            "start": walls[i].doors[j].position - 1,
+                            "end": walls[i].doors[j].fwidth + 1
+                        };
+                    }
+                    else {
+                        freeSpace = {
+                            "wall": i,
+                            "start": 0,
+                            "end": walls[i].doors[j].fwidth + 1
+                        };
+                    }
+                    availableSpaces[freeSpaceCounter] = freeSpace;
+                    freeSpaceCounter++;
+                }
+            }
+        }
+    }
+
+    for (var k = 0; k < availableSpaces.length; k++) {
+        var availableSpace = availableSpaces[k].end - availableSpaces[k].start;
+        var windowSize = MAX_WINDOW_WIDTH;
+        var tryAgain = 1;
+
+        if (availableSpaces[k] >= MIN_MOD_WIDTH) { //if there's enough space to fit a min size window
+            while (!validateModSize(availableSpace, (windowSize / tryAgain) + 2, tryAgain, availableSpaces[k].wall), availableSpaces[k].start) { //keep trying until windows fit in the space (with min filler)
+                tryAgain++; //used to divide the window size by 2 at each try to try smaller window sizes
+            }
+        }
+    }
+}
+
+function validateWindowModSize(space, size, number, wall, start) {
+
+    if (space >= MIN_MOD_WIDTH) {
+        if (size > space) {
+            size = size / 2;
+            validateWindowModSize(space, size, number, wall, start);
+        }
+        else if (size < space) {
+            var tempSize = validateDecimal(size); //make sure the size is 
+            while (tempSize < space) {
+                var multiplier = 1;
+                tempSize = size * multiplier;
+                multiplier++;
+                if (tempSize === space) {
+                    fillMods(tempSize, multiplier, wall, 0, start);
+                }
+                else if (tempSize > space) {
+                    validateWindowModSize(space, tempSize, multiplier, wall, start);
+                }
+                else if ((space - tempSize) <= MIN_MOD_WIDTH) {
+                    fillMods(tempSize, number, wall, space - tempSize, start);
+                }
+            }
+        }
+        else { //size === space
+            fillMods(tempSize, multiplier, wall, 0, start);
+        }
+    }
+    else { //space < MIN_MOD_SIZE
+        fillFiller(space, wall, start);
+    }
+}
+
+
+function fillMods(size, number, wall, filler, start) {
+    var mod;
+
+    if (filler > 0)
+        fillFiller(filler / 2, wall, start);
+
+    for (var i = 0; i < number; i++) {
+        mod = {
+            type: "Window",
+            width: size / number,
+            wall: wall,
+            startHeight: findCurrentWallHeight(start + (filler / 2), wall),
+            endHeight: findCurrentWallHeight(start + (filler / 2) + (size / number), wall),
+            position: start + (filler / 2),
+            kneewallType: kneewallType,
+            kneewallHeight: kneewallHeight,
+            transomType: transomType,
+            transomHeight: transomHeight,
+            window: []
+        }
+        start = start + (size / number);
+
+
+
+        insertMod(mod, walls[wall]);
+    }
+
+    if (filler > 0)
+        fillFiller(filler / 2, wall, start);
+}
+
+function fillFiller(filler, wall, start) {
+    var mod;
+
+    mod = {
+        type: "Filler",
+        width: filler,
+        wall: wall,
+        startHeight: findCurrentWallHeight(start, wall),
+        endHeight: findCurrentWallHeight(start + filler, wall),
+        position: start
+    }
+    insertMod(mod, walls[wall]);
+}
+
+
+/************************************************************************************************************************************/
+/************************************************************************************************************************************/
+/************************************************************************************************************************************/
