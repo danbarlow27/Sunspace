2074b971603bb4f1a9428387efe8a0839ad7403f
danbarlow27 <danbarlow27@gmail.com>
2013-08-26 15:20:57 -0400

    new project validation

--- SunspaceDealerDesktop/NewProject.aspx.designer.cs
+++ SunspaceDealerDesktop/NewProject.aspx.designer.cs
@@ -2011,6 +2011,15 @@ namespace SunspaceDealerDesktop {
         protected global::System.Web.UI.WebControls.Label lblQuestion8PagerAnswer;
         
         /// <summary>
+        /// txtErrorMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTextArea txtErrorMessage;
+        
+        /// <summary>
         /// hidCountry control.
         /// </summary>
         /// <remarks>
