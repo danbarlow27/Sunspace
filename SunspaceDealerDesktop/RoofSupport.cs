1a56d4d8e4e9a1e31955353fae5307b02d1ea26c
patoui <patrique.ouimet@gmail.com>
2013-12-12 00:29:49 -0500

    Fixing commit errors, adding new RoofSupport.cs file

---
+++ SunspaceDealerDesktop/RoofSupport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SunspaceDealerDesktop
+{
+    public class RoofSupport
+    {
+        #region Attributes
+        private double height;
+        #endregion
+
+        public RoofSupport() {
+            height = 0;
+        }
+
+        public RoofSupport(double sentHeight)
+        {
+            height = sentHeight;
+        }
+    }
+}
