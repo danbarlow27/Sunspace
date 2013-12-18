95a9ab2e0f420c2e6516f897d3d45935ae883e9d
patoui <patrique.ouimet@gmail.com>
2013-12-18 09:36:16 -0500

    Removed unused field on WizardRoofOnly.aspx

--- SunspaceDealerDesktop/WizardRoofOnly.aspx.designer.cs
+++ SunspaceDealerDesktop/WizardRoofOnly.aspx.designer.cs
@@ -56,14 +56,5 @@ namespace SunspaceDealerDesktop {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlTextArea txtErrorMessage;
-        
-        /// <summary>
-        /// hiddenFieldsDiv control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl hiddenFieldsDiv;
     }
 }
