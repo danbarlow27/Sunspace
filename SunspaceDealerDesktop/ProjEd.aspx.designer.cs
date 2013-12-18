fa46b84a2bcebe992725b24179b962bd4e79b597
tamjd1 <taha.amjad@uoit.net>
2013-12-17 19:04:55 -0500

    lots of db and backend work on projEd, fixing up linearItem moduleItem wall classes

--- SunspaceDealerDesktop/ProjEd.aspx.designer.cs
+++ SunspaceDealerDesktop/ProjEd.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace SunspaceDealerDesktop {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.PlaceHolder accordion;
+        
+        /// <summary>
+        /// sdsDBConnection control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource sdsDBConnection;
     }
 }
