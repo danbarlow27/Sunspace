0837d6828edeb3204296092c9dcf753119b42970
Kyle Brougham <brougham.kyle@gmail.com>
2013-12-19 10:47:16 -0500

    JSON stuff

---
+++ SunspaceDealerDesktop/JSONTest.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="JSONTest.aspx.cs" Inherits="SunspaceDealerDesktop.JSONTest" %>
+
+<!DOCTYPE html>
+<script>
+</script>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lblJSON" runat="server">Test Failed</asp:Label>
+    </div>
+    
+        <asp:TextBox ID="txtShit" runat="server" OnChange="runme()"></asp:TextBox>
+        <asp:Button ID="btnFuck" runat="server" OnClick="btnFuck_Click" />
+    </form>
+
+    <input id="hidRealHidden" type="hidden" runat="server" />
+</body>
+</html>
---
+++ SunspaceDealerDesktop/JSONTest2.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace SunspaceDealerDesktop
+{
+    public partial class JSONTest2 : System.Web.UI.Page
+    {
+        string json;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Wall aWall = new Wall();
+            aWall.Name = "Apple";
+            aWall.StartHeight = 133;
+            aWall.EndHeight = 133;
+            aWall.FireProtection = false;
+            aWall.GablePeak = 155;
+            aWall.Length = 133;
+
+            json = JsonConvert.SerializeObject(aWall);
+            hidRealHidden.Value = json;
+        }
+
+        protected void btnFuck_Click(object sender, EventArgs e)
+        {
+            Wall aWall2 = JsonConvert.DeserializeObject<Wall>(Request.Form[hidRealHidden.UniqueID].ToString());
+            string temp;
+        }
+    }
+}
