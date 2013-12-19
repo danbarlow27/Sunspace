85de3457421ba02c115ac93a3eece2ca75e7ab38
tamjd1 <taha.amjad@uoit.net>
2013-12-19 03:22:14 -0500

    uneven vents completed fully with validation

--- SunspaceDealerDesktop/WizardWindowsOnly.aspx.designer.cs
+++ SunspaceDealerDesktop/WizardWindowsOnly.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace SunspaceDealerDesktop {
         protected global::System.Web.UI.WebControls.PlaceHolder lblWindowPager;
         
         /// <summary>
+        /// lblErrorMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblErrorMessage;
+        
+        /// <summary>
         /// txtErrorMessage control.
         /// </summary>
         /// <remarks>
